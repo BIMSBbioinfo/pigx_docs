5b803f0539d9826949e8d6512183d8c825a04774
Ricardo Wurmus <rekado@elephly.net>
2020-08-11 08:28:02 +0200

    Generate page title.

--- build.sh
+++ build.sh
@@ -3,9 +3,11 @@ sed -i book/SUMMARY.html -e 's|\.md|.html|'
 
 for file in $(ls -1 pages); do
     echo $file
+    title=$(egrep --max-count=1 '^# ' pages/$file | tail -c +3)
     pandoc -f markdown_github+smart \
            -t html \
            -V navigation="$(cat book/SUMMARY.html)" \
+           -V pagetitle="$title" \
            -o "book/$(basename $file .md).html" \
            --template template.html pages/$file
 done
